062ddbf0264d0d07e6cf0234312a5d5e26d0dee4
Umang-Gupta-420 <umangg371@gmail.com>
2024-07-23 18:32:46 +0530

    Add files via upload

---
+++ init/index.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Listing = require("../models/listing.js");
+const initData = require("./data.js");
+
+main().then(() => {
+    console.log("connected to DB")
+})
+.catch((err) => {
+    console.log(err)
+})
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/traveling")
+}
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("data was initalized");
+}
+
+initDB();
